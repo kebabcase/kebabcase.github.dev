acc3eb50b0a9cfe9033e78c2b97bd7716ae0a7ee
Jae Hyun Choe <andrew00917@yahoo.com>
2018-04-26 22:54:29 -0400

    chore(lost-in-maze): remove
    Remove `lost-in-maze` component from the project for now.

--- src/components/lost-in-maze/direction/direction.ts
+++
@@ -1,6 +0,0 @@
-export const DIRECTION = {
-  LEFT: 'LEFT',
-  TOP: 'TOP',
-  RIGHT: 'RIGHT',
-  BOTTOM: 'BOTTOM',
-};
--- src/components/lost-in-maze/path/path.d.ts
+++
@@ -1,6 +0,0 @@
-import {Point} from '../tile/tile.d';
-
-export type Path = {
-  path: Point[];
-  isOnPath: (point: Point) => boolean;
-};
